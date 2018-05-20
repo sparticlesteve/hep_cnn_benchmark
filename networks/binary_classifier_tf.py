50d68931d043cc3719618364a3d9d12bb9f950ef
Steve Farrell <sfarrell@cern.ch>
2018-05-20 14:43:46 -0700

    typo bugfix

--- networks/binary_classifier_tf.py
+++ networks/binary_classifier_tf.py
@@ -1,7 +1,7 @@
 # System imports
 from __future__ import print_function
 from __future__ import division
-from __Future__ import absolute_import
+from __future__ import absolute_import
 import os
 
 # Data libraries
