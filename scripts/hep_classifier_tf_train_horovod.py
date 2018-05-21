28c729a873f2cb27784e748c82815f3c23644498
Steve Farrell <sfarrell@cern.ch>
2018-05-21 11:11:33 -0700

    started cleaning horovod training script

--- scripts/hep_classifier_tf_train_horovod.py
+++ scripts/hep_classifier_tf_train_horovod.py
@@ -1,37 +1,3 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-#*** License Agreement ***                                                                                                                                                                                                                                                                                  
-#                                                                                                                                                                                                                                                                                                           
-#High Energy Physics Deep Learning Convolutional Neural Network Benchmark (HEPCNNB) Copyright (c) 2017, The Regents of the University of California,                                                                                                                                                        
-#through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy). All rights reserved.                                                                                                                                                           
-#                                                                                                                                                                                                                                                                                                           
-#Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:                                                                                                                                                             
-#(1) Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.                                                                                                                                                                           
-#(2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer                                                                                                                                                                         
-#in the documentation and/or other materials provided with the distribution.                                                                                                                                                                                                                                
-#(3) Neither the name of the University of California, Lawrence Berkeley National Laboratory, U.S. Dept. of Energy nor the names                                                                                                                                                                            
-#of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.                                                                                                                                                                       
-#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,                                                                                                                                                                              
-#BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE                                                                                                                                                                   
-#COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT                                                                                                                                                           
-#LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF                                                                                                                                                      
-#LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,                                                                                                                                                          
-#EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-#You are under no obligation whatsoever to provide any bug fixes, patches, or upgrades to the features,                                                                                                                                                                                                     
-#functionality or performance of the source code ("Enhancements") to anyone; however,                                                                                                                                                                                                                       
-#if you choose to make your Enhancements available either publicly, or directly to Lawrence Berkeley National Laboratory,                                                                                                                                                                                   
-#without imposing a separate written license agreement for such Enhancements, then you hereby grant the following license: a non-exclusive,                                                                                                                                                                 
-#royalty-free perpetual license to install, use, modify, prepare derivative works, incorporate into other computer software,                                                                                                                                                                                
-#distribute, and sublicense such enhancements or derivative works thereof, in binary and source code form.                                                                                                                                                                                                  
-#---------------------------------------------------------------      
-
-
-# In[ ]:
-
 #os stuff
 import os
 import sys
@@ -49,32 +15,18 @@ import time
 import numpy as np
 
 #tensorflow
-sys.path.append("/usr/common/software/tensorflow/intel-tensorflow/head/lib/python2.7/site-packages")
-sys.path.append("/global/homes/t/tkurth/.conda/envs/thorstendl-horovod/lib/python2.7/site-packages")
 import tensorflow as tf
 import tensorflow.contrib.keras as tfk
 import horovod.tensorflow as hvd
 
 #slurm helpers
-sys.path.append("../")
 import slurm_tf_helper.setup_clusters as sc
 
 #housekeeping
 import networks.binary_classifier_tf as bc
 
-#debugging
-#tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-
-
-# In[ ]:
-
 # Initialize Horovod
 hvd.init()
-
-
-# # Useful Functions
-
-# In[ ]:
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -138,8 +90,6 @@ def parse_arguments():
     
     return args
 
-
-# In[ ]:
 
 def train_loop(sess,train_step,global_step,optlist,args,trainset,validationset):
     
@@ -272,14 +222,10 @@ def train_loop(sess,train_step,global_step,optlist,args,trainset,validationset):
 
 # # Parse Parameters
 
-# In[ ]:
-
 args = parse_arguments()
 
 
 # # Multi-Node Stuff
-
-# In[ ]:
 
 #decide who will be worker and who will be parameters server
 if args['num_tasks'] > 1:
@@ -310,8 +256,6 @@ else:
 
 
 # # On-Node Stuff
-
-# In[ ]:
 
 if (args['node_type'] == 'worker'):
     #common stuff
@@ -350,8 +294,6 @@ if (args['node_type'] == 'worker'):
 
 # ## Build Network and Functions
 
-# In[ ]:
-
 if args['node_type'] == 'worker':
     print("Rank",args["task_index"],":","Building model")
     args['device'] = tf.train.replica_device_setter(worker_device="/job:worker/task:%d" % args['task_index'],
@@ -369,8 +311,6 @@ if args['node_type'] == 'worker':
 
 
 # ## Setup Iterators
-
-# In[ ]:
 
 if args['node_type'] == 'worker':
     print("Rank",args["task_index"],":","Setting up iterators")
@@ -397,16 +337,12 @@ print("Stopping after %d global steps"%(args["last_step"]))
 
 # # Train Model
 
-# In[ ]:
-
 #determining which model to load:
 metafilelist = [args['modelpath']+'/'+x for x in os.listdir(args['modelpath']) if x.endswith('.meta')]
 if not metafilelist:
     #no model found, restart from scratch
     args['restart']=True
 
-
-# In[ ]:
 
 #initialize session
 if (args['node_type'] == 'worker'):
@@ -468,9 +404,3 @@ if (args['node_type'] == 'worker'):
                 train_loop(sess,train_step,global_step,optlist,args,trainset,validationset)
                 total_time -= time.time()
                 print("FINISHED Training. Total time %g"%(total_time))
-
-
-# In[ ]:
-
-
-
