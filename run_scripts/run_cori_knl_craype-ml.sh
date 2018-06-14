4fa528bbf51a41e7046c0ad3ccdaf55abb22a190
Steve Farrell <sfarrell@cern.ch>
2018-06-14 10:01:02 -0700

    WIP cray plugin script updates

--- run_scripts/run_cori_knl_craype-ml.sh
+++ run_scripts/run_cori_knl_craype-ml.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -q regular
+#SBATCH -q debug
 #SBATCH -C knl,quad,cache
 #SBATCH -t 0:30:00
 #SBATCH -J hep_train_tf
@@ -47,19 +47,19 @@
 #---------------------------------------------------------------      
 
 
-# TODO : THIS SCRIPT NEEDS TO BE UPDATED
-
-#use custom craype-ml installation
-module use /global/homes/t/tkurth/custom_rpm
-
-#set up python stuff
+# Environment setup
+module load gcc/6.3.0
 module load tensorflow/intel-head
-module use /global/homes/t/tkurth/custom_rpm/modulefiles
-module load craype-ml-plugin-py2/1.1.0
+module load craype-ml-plugin-py2/1.0.1
+export PYTHONPATH=$PWD:$PYTHONPATH
 
-#better binding
+# Binding settings
 #bindstring="numactl -C 1-67,69-135,137-203,205-271"
 bindstring=""
 
-#run
-srun -N ${SLURM_NNODES} -n ${SLURM_NNODES} -c 272 -u ${bindstring} python scripts/hep_classifier_tf_train_craype-ml.py --config=configs/cori_knl_224_tune.json --num_tasks=${SLURM_NNODES} > hep_224x224_knl-craype-ml-plugin-2t_w$(( ${SLURM_NNODES} ))_p0.out 2>&1
+# Run the training
+srun -N ${SLURM_NNODES} -n ${SLURM_NNODES} -c 272 -u ${bindstring} \
+    python scripts/hep_classifier_tf_train_craype-ml.py \
+    --config=configs/cori_knl_224_tune.json \
+    --num_tasks=${SLURM_NNODES}
+#> hep_224x224_knl-craype-ml-plugin-2t_w$(( ${SLURM_NNODES} ))_p0.out 2>&1
