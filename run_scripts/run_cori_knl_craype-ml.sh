4f65a7aa4df331c230e6a113aaf6b5231846c12d
Steve Farrell <sparticlesteve@gmail.com>
2018-10-08 09:27:59 -0700

    removed everything not needed for blog post

--- run_scripts/run_cori_knl_craype-ml.sh
+++ run_scripts/run_cori_knl_craype-ml.sh
@@ -61,6 +61,5 @@ bindstring=""
 # Run the training
 srun -N ${SLURM_NNODES} -n ${SLURM_NNODES} -c 272 -u ${bindstring} \
     python scripts/hep_classifier_tf_train_craype-ml.py \
-    --config=configs/cori_knl_224_adam.json \
+    --config=configs/cori_knl_224.json \
     --num_tasks=${SLURM_NNODES}
-#> hep_224x224_knl-craype-ml-plugin-2t_w$(( ${SLURM_NNODES} ))_p0.out 2>&1
